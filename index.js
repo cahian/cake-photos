be8926aa4c5ef800a6997a704a2a7828ba50b688
Cahian Freire <cahianfreire@gmail.com>
2023-11-25 17:50:29 -0300

    Remove serverless support

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const serverless = require("serverless-http");
+const cake = require("./cake.js");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const cake = require("./cake.js");
 
 app.use("/cake", cake);
 
@@ -11,10 +10,6 @@ app.get("/", (req, res) => {
   res.send("Nothing here.");
 });
 
-if (process.env.ENVIRONMENT === 'production') {
-  exports.handler = serverless(app);
-} else {
-  app.listen(port, () => {
-    console.log(`Cahian's app listening on port ${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Cahian's app listening on port ${port}`);
+});
